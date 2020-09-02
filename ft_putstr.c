bf1100cb5fa9d4e39c23d18118e3d66866859418
Josefiina <josefiina.holopainen@gmail.com>
2020-09-02 14:31:38 +0300

    created putstr and strlen

---
+++ ft_putstr.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jholopai <jholopai@hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/02 14:24:16 by jholopai          #+#    #+#             */
+/*   Updated: 2020/09/02 14:27:58 by jholopai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+/*
+** Prints out the string given as parameter.
+** Allowed functions: ft_purchar.
+*/
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
